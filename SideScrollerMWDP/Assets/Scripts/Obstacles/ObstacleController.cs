dcbf0e17e42574a8be1de37c900da12583f793ed
Daniel Pankowski <tajz3r@gmail.com>
2020-02-12 20:53:42 +0100

    Add Player interaction with obstacle

--- SideScrollerMWDP/Assets/Scripts/Obstacles/ObstacleController.cs
+++ SideScrollerMWDP/Assets/Scripts/Obstacles/ObstacleController.cs
@@ -2,8 +2,9 @@
 
 public class ObstacleController : MonoBehaviour
 {
+    float damage = 50;
     private void OnCollisionEnter(Collision collision)
     {
-        
+        collision.gameObject.GetComponent<PlayerController>().GetHit(damage);
     }
 }
--- SideScrollerMWDP/Assets/Scripts/Player/PlayerController.cs
+++ SideScrollerMWDP/Assets/Scripts/Player/PlayerController.cs
@@ -5,9 +5,12 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private Text gameOverText;
+    [SerializeField]
+    private Rigidbody rigidbody;
 
     private const float maxHealth = 100;
     private float currentHealth;
+    private bool gameOver = false;
 
     private void Awake()
     {
@@ -16,7 +19,12 @@ public class PlayerController : MonoBehaviour
 
     public void GetHit(float damage)
     {
-        //1. Otrzymywanie obrazen
-        //2. GameOver <= 0
+        currentHealth -= damage;
+        rigidbody.velocity = new Vector3(rigidbody.velocity.x, 200 * Time.deltaTime);
+        if (currentHealth <= 0)
+        {
+            gameOver = true;
+        }
+        gameOverText.gameObject.SetActive(gameOver);
     }
 }
